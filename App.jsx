1983d6b78cead877da8b6db4a752d0714c0afb51
Henry Zhao <henry.zhao19931004@gmail.com>
2017-01-24 17:25:22 -0500

    first styling

--- App.jsx
+++ App.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Letter from './Components/Letter.js';
+
 class App extends React.Component {
-   render() {
-      return (
+    render() {
+        
+        const containerStyle = {
+            padding: 10,
+            margin: 10,
+            backgroundColor: "#DEB887",
+            color: "#333",
+            display: "inline-block",
+            fontFamily: "monospace",
+            fontSize: 32,
+            textAlign: "center",
+        };
+        
+        
+        return (
          <div>
-            <h1>Homepage</h1>
-            <p>
-            <Link to="/">Home</Link>
-            <Link to="about">About</Link>
-            <Link to="contact">Contact</Link>
-            </p>
+            <h1 style={containerStyle}>THE</h1>
+
+            <Letter bgcolor="#58B3FF">W</Letter>
+            <Letter bgcolor="#FF605F">O</Letter>
+            <Letter bgcolor="#FFD52E">R</Letter>
+            <Letter bgcolor="#49DD8E">R</Letter>
+            <Letter bgcolor="#AE99FF">D</Letter>
+
+            <h1 style={containerStyle}>Game</h1>
+
+            <ul>
+                <li><Link to="about">About</Link></li>
+                <li><Link to="contact">Contact</Link></li>
+            </ul>
 
             {this.props.children}
          </div>
